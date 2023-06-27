199a6c69d9a3bf7559bd035192829eec7742739d
Haorsis <cadetgauthier3@gmail.com>
2023-06-27 14:45:14 +0200

    leeeeet's gooo

--- flutter_TP03/pomme_poire_ananas/lib/vues/counter_item.dart
+++ flutter_TP03/pomme_poire_ananas/lib/vues/counter_item.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class CounterItem extends StatefulWidget {
@@ -13,7 +15,43 @@ class _CounterItem extends State<CounterItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.greenAccent,
-        child: Center(child: Text(widget.counter.toString())));
+        color: widget.counter % 2 == 1 ? Colors.green : Colors.amber,
+        child: Center(
+            child: Column(
+          children: [
+            Image.asset(
+              getImageByCounter(),
+              height: 200,
+              width: 200,
+            ),
+            Text(
+              widget.counter.toString(),
+              style: const TextStyle(color: Colors.white),
+            )
+          ],
+        )));
+  }
+
+  String getImageByCounter() {
+    if (isPremier()) {
+      return "assets/images/ananas.png";
+    }
+    if (widget.counter % 2 == 0) {
+      return "assets/images/poire.png";
+    } else {
+      return "assets/images/pomme.png";
+    }
+  }
+
+  bool isPremier() {
+    if (widget.counter <= 1) {
+      return false;
+    }
+    for (int i = 2; i <= sqrt(widget.counter); i++) {
+      if (widget.counter % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 }
