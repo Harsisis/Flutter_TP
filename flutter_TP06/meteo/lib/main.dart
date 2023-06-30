e385d3fc924082ab6952db3c3f96e7ebb534d4a0
Haorsis <cadetgauthier3@gmail.com>
2023-06-30 14:09:39 +0200

    init meteo project

---
+++ flutter_TP06/meteo/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:meteo/views/meteo_screen.dart';
+
+void main() {
+  runApp(const Meteo());
+}
---
+++ flutter_TP06/meteo/lib/views/meteo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Meteo extends StatelessWidget {
+  const Meteo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Finder",
+    );
+  }
+}
